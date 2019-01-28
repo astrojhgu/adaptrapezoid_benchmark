f0c8af8a2cc612a1784ada0efb790ce7e3b52542
Gu Junhua <jhgu@nao.cas.cn>
2019-01-28 19:14:01 +0800

    modified:   main.jl

--- jl/main.jl
+++ jl/main.jl
@@ -1,26 +1,25 @@
 #!/usr/bin/env julia
 
-struct Point{T<:AbstractFloat}
+struct Point{T}
     x::T
     f::T
-    #Point{T}(x::T, f::T)=new{T}(x::T, f::T)
 end
 
-function integrate(func::Function, ticks::Array{T,1}, eps::T)::T where T<:AbstractFloat
+function integrate(func::Function, ticks::Array{T,1}, eps::T)::T where T<:AbstractFloat 
     if length(ticks)<2
         return 0.0
     end
     points::Array{Point{T}, 1}=[Point(x, func(x)) for x in ticks]
     full_width=last(ticks)-first(ticks)
-    areas::Array{T,1}=[]
+    area=0.0
     right=last(points)
     sz=length(points)
     while sz>1 
         left=points[lastindex(points)-1]
-        mid=(left.x+right.x)/2
+        mid=(left.x+right.x)/2.0
         fmid=func(mid)
         if abs(left.f+right.f-fmid*2.0)<=eps
-            push!(areas, (left.f+right.f+fmid*2.0)*(right.x-left.x)/4.0)
+            area+=(left.f+right.f+fmid*2.0)*(right.x-left.x)/4.0
             pop!(points)
             right=left
             sz-=1
@@ -31,10 +30,7 @@ function integrate(func::Function, ticks::Array{T,1}, eps::T)::T where T<:Abstra
             sz+=1
         end
     end
-    areas=sort!(areas, by=x->abs(x))
-    sum(areas)
+    area
 end
 
-#for i in 1:100
-#@time for i in 1:100;  println(integrate(x->sin(x^2), [0.0, 1.0, 2.0, sqrt(8*pi)], 1e-10)); end
-#end
+@time for i in 1:100;  println(integrate(x->sin(x^2), [0.0, 1.0, 2.0, sqrt(8*pi)], 1e-10)); end
