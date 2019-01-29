0e86578d552979929ba8fcd1c996f0107f96ea69
Gu Junhua <jhgu@nao.cas.cn>
2019-01-29 19:06:34 +0800

    new file:   _clang-format 	modified:   adapt.hpp formatted with clang-format 	modified:   main.cpp formatted with clang-format

--- cpp/adapt.hpp
+++ cpp/adapt.hpp
@@ -8,87 +8,102 @@
 #include <utility>
 #include <vector>
 #include <stack>
- 
-template <typename T, typename F>
-T integrate(F f, T eps,  std::vector<T> ticks) {
-  if (ticks.size() <= 1) {
-    return T{};
-  }
-  constexpr T kHalf = T{0.5}, kQuarter = T{0.25};
-  constexpr T kTwo = T{2}, kFour = T{4};
-  eps = eps * kFour / (ticks.back() - ticks.front());
 
-  std::vector<T> areas;
+template <typename T, typename F> T integrate (F f, T eps, std::vector<T> ticks)
+{
+    if (ticks.size () <= 1)
+        {
+            return T{};
+        }
+    constexpr T kHalf = T{ 0.5 }, kQuarter = T{ 0.25 };
+    constexpr T kTwo = T{ 2 }, kFour = T{ 4 };
+    eps = eps * kFour / (ticks.back () - ticks.front ());
 
-  std::stack<T> ss;
+    std::vector<T> areas;
 
-  auto right = ticks.back();
-  auto right_f = f(right);
-  ticks.pop_back();
+    std::stack<T> ss;
 
-  while (!ticks.empty()) {
-    ss.push(f(ticks.back()));
+    auto right = ticks.back ();
+    auto right_f = f (right);
+    ticks.pop_back ();
 
-    while (!ss.empty()) {
-      auto left = ticks.back();
-      auto left_f = ss.top(); 
-      
-      T mid = (left + right) * kHalf, fmid = f(mid);
-      if (std::abs(left_f + right_f - kTwo * fmid) <= eps) {
-        areas.push_back((left_f + right_f + fmid * kTwo) * (right - left) * kQuarter);
-        right = left; right_f = left_f;
-        ss.pop();
-        ticks.pop_back();
-      } else {
-        ticks.push_back(mid);
-        ss.push(fmid);
-      }
-    }
-  }
-  
-  std::stable_sort(areas.begin(), areas.end(), [](auto x, auto y){return std::abs(x)<std::abs(y);});
+    while (!ticks.empty ())
+        {
+            ss.push (f (ticks.back ()));
 
-  return std::accumulate(areas.begin(), areas.end(), static_cast<T>(0));
+            while (!ss.empty ())
+                {
+                    auto left = ticks.back ();
+                    auto left_f = ss.top ();
+
+                    T mid = (left + right) * kHalf, fmid = f (mid);
+                    if (std::abs (left_f + right_f - kTwo * fmid) <= eps)
+                        {
+                            areas.push_back ((left_f + right_f + fmid * kTwo) * (right - left) * kQuarter);
+                            right = left;
+                            right_f = left_f;
+                            ss.pop ();
+                            ticks.pop_back ();
+                        }
+                    else
+                        {
+                            ticks.push_back (mid);
+                            ss.push (fmid);
+                        }
+                }
+        }
+
+    std::stable_sort (areas.begin (), areas.end (),
+                      [](auto x, auto y) { return std::abs (x) < std::abs (y); });
+
+    return std::accumulate (areas.begin (), areas.end (), static_cast<T> (0));
 }
 
 
-template <typename T, typename F>
-T integrate_nosort(F f, T eps, std::vector<T> ticks) {
-   if (ticks.size() <= 1) {
-    return T{};
-  }
-  constexpr T kHalf = T{0.5}, kQuarter = T{0.25};
-  constexpr T kTwo = T{2}, kFour = T{4};
-  eps = eps * kFour / (ticks.back() - ticks.front());
+template <typename T, typename F> T integrate_nosort (F f, T eps, std::vector<T> ticks)
+{
+    if (ticks.size () <= 1)
+        {
+            return T{};
+        }
+    constexpr T kHalf = T{ 0.5 }, kQuarter = T{ 0.25 };
+    constexpr T kTwo = T{ 2 }, kFour = T{ 4 };
+    eps = eps * kFour / (ticks.back () - ticks.front ());
 
-  T result{};
+    T result{};
 
-  std::stack<T> ss;
+    std::stack<T> ss;
 
-  auto right = ticks.back();
-  auto right_f = f(right);
-  ticks.pop_back();
+    auto right = ticks.back ();
+    auto right_f = f (right);
+    ticks.pop_back ();
 
-  while (!ticks.empty()) {
-    ss.push(f(ticks.back()));
+    while (!ticks.empty ())
+        {
+            ss.push (f (ticks.back ()));
 
-    while (!ss.empty()) {
-      auto left = ticks.back();
-      auto left_f = ss.top(); 
-      
-      T mid = (left + right) * kHalf, fmid = f(mid);
-      if (std::abs(left_f + right_f - kTwo * fmid) <= eps) {
-        result += (left_f + right_f + fmid * kTwo) * (right - left) * kQuarter;
-        right = left; right_f = left_f;
-        ss.pop();
-        ticks.pop_back();
-      } else {
-        ticks.push_back(mid);
-        ss.push(fmid);
-      }
-    }
-  }
-  return result;
+            while (!ss.empty ())
+                {
+                    auto left = ticks.back ();
+                    auto left_f = ss.top ();
+
+                    T mid = (left + right) * kHalf, fmid = f (mid);
+                    if (std::abs (left_f + right_f - kTwo * fmid) <= eps)
+                        {
+                            result += (left_f + right_f + fmid * kTwo) * (right - left) * kQuarter;
+                            right = left;
+                            right_f = left_f;
+                            ss.pop ();
+                            ticks.pop_back ();
+                        }
+                    else
+                        {
+                            ticks.push_back (mid);
+                            ss.push (fmid);
+                        }
+                }
+        }
+    return result;
 }
 
 
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,26 +8,35 @@
 #include <benchmark/benchmark.h>
 
 
-double foo(double x) { return std::sin(x * x); }
-
-void run(benchmark::State& state) {
-  constexpr double PI = 3.14159265358979323846;
-  //std::cout<<"Result 1="<<std::setprecision(10)<<integrate(foo, 1e-10, std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)})<<std::endl;
-  for(auto _: state){
-    integrate(foo, 1e-10, std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)});
-  }
+double foo (double x)
+{
+    return std::sin (x * x);
 }
 
-void run_nosort(benchmark::State& state) {
-  constexpr double PI = 3.14159265358979323846;
-  //std::cout<<"Result 2="<<std::setprecision(10)<<integrate_nosort(foo, 1e-10, std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)})<<std::endl;
+void run (benchmark::State &state)
+{
+    constexpr double PI = 3.14159265358979323846;
+    // std::cout<<"Result 1="<<std::setprecision(10)<<integrate(foo, 1e-10,
+    // std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)})<<std::endl;
+    for (auto _ : state)
+        {
+            integrate (foo, 1e-10, std::vector<double>{ 0.0, 1.0, 2.0, std::sqrt (8.0 * PI) });
+        }
+}
 
-  for(auto _: state){
-    integrate_nosort(foo, 1e-10, std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)});
-  }
+void run_nosort (benchmark::State &state)
+{
+    constexpr double PI = 3.14159265358979323846;
+    // std::cout<<"Result 2="<<std::setprecision(10)<<integrate_nosort(foo, 1e-10,
+    // std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)})<<std::endl;
+
+    for (auto _ : state)
+        {
+            integrate_nosort (foo, 1e-10, std::vector<double>{ 0.0, 1.0, 2.0, std::sqrt (8.0 * PI) });
+        }
 }
 
 
-BENCHMARK(run);
-BENCHMARK(run_nosort);
-BENCHMARK_MAIN();
+BENCHMARK (run);
+BENCHMARK (run_nosort);
+BENCHMARK_MAIN ();
