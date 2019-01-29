ad85ee31f768ea2deed739e9587752e043a32941
Gu Junhua <jhgu@nao.cas.cn>
2019-01-29 21:02:19 +0800

    modified:   rs/benches/bench.rs updated benchmark config--decreased the number of sampling

--- rs/benches/bench.rs
+++ rs/benches/bench.rs
@@ -10,11 +10,8 @@ use num_traits::float::FloatConst;
 
 
 fn run(c:&mut Criterion){
-    let mut cnt=0;
     c.bench_function("with sorting", move|b|{
         b.iter(||{
-            cnt+=1;
-            println!("{}", cnt);
             integrate(
                 &|x: f64| x.powi(2).sin(),
                 1e-10,
@@ -25,10 +22,8 @@ fn run(c:&mut Criterion){
 }
 
 fn run_nosort(c:&mut Criterion){
-    let mut cnt=0;
     c.bench_function("no sorting", move|b|{
         b.iter(||{
-            cnt+=1;
             integrate_nosort(
                 &|x: f64| x.powi(2).sin(),
                 1e-10,
@@ -38,6 +33,6 @@ fn run_nosort(c:&mut Criterion){
     });
 }
 
-criterion_group!(benches, run, run_nosort);
+criterion_group!(name=benches; config=Criterion::default().sample_size(30).nresamples(10); targets=run, run_nosort);
 criterion_main!(benches);
 
