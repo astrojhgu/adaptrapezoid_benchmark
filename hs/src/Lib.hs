e249aa16203fda6aa20f1052f6c8dc843a463e49
Brandon Simmons <brandon.m.simmons@gmail.com>
2019-02-05 12:37:17 -0500

    Mark INLINABLE exported functions, INLINE iter functions
    Before
    
         benchmarking integration/sort
         time                 3.516 s    (3.400 s .. 3.633 s)
                              1.000 R²   (0.999 R² .. 1.000 R²)
         mean                 3.494 s    (3.476 s .. 3.515 s)
         std dev              24.50 ms   (387.6 μs .. 30.18 ms)
         variance introduced by outliers: 19% (moderately inflated)
    
         benchmarking integration/no sort
         time                 2.165 s    (1.907 s .. 2.445 s)
                              0.997 R²   (0.997 R² .. 1.000 R²)
         mean                 2.169 s    (2.097 s .. 2.236 s)
         std dev              85.27 ms   (41.67 ms .. 107.3 ms)
         variance introduced by outliers: 19% (moderately inflated)
    
    After
    
         benchmarking integration/sort
         time                 1.752 s    (1.558 s .. 1.857 s)
                              0.999 R²   (0.998 R² .. 1.000 R²)
         mean                 1.737 s    (1.673 s .. 1.765 s)
         std dev              48.09 ms   (17.62 ms .. 66.33 ms)
         variance introduced by outliers: 19% (moderately inflated)
    
         benchmarking integration/no sort
         time                 1.138 s    (902.1 ms .. 1.356 s)
                              0.992 R²   (0.992 R² .. 1.000 R²)
         mean                 1.089 s    (1.020 s .. 1.119 s)
         std dev              57.91 ms   (21.87 ms .. 67.92 ms)
         variance introduced by outliers: 19% (moderately inflated)

--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -14,6 +14,7 @@ module Lib (integrate, integrate_nosort)
     area (Point x1 f1) (Point x2 f2)=(f1+f2)*(x2-x1)/(fromInteger 2)
     
 
+    {-# INLINE integrate_iter #-}
     integrate_iter::(Fractional a, Ord a)=>(a->a)->a->[Point a]->[a]->([Point a], [a])
     integrate_iter func eps (left@(Point _ f1):right@(Point _ f2):others) areas
                 | abs (f1+f2-fm*(fromInteger 2)) <= eps = integrate_iter func eps (right:others) ((area left right):areas)
@@ -21,6 +22,7 @@ module Lib (integrate, integrate_nosort)
                 where (Point xm fm)=midpoint func left right
     integrate_iter _ _ points areas = (points, areas)
         
+    {-# INLINABLE integrate #-}
     integrate::(Fractional a, Ord a)=>(a->a)->[a]->a->a
     integrate func ticks eps=foldr (+) (fromInteger 0) sorted_areas
             where 
@@ -29,6 +31,7 @@ module Lib (integrate, integrate_nosort)
                 areas=snd $ integrate_iter func eps1 points []
                 sorted_areas=sortBy (\a b->compare (abs a) (abs b)) areas
 
+    {-# INLINE integrate_iter_nosort #-}
     integrate_iter_nosort::(Fractional a, Ord a)=>(a->a)->a->[Point a]->a->([Point a], a)
     integrate_iter_nosort func eps (left@(Point _ f1):right@(Point _ f2):others) old_area
                 | abs (f1+f2-fm*(fromInteger 2)) <= eps = integrate_iter_nosort func eps (right:others) ((area left right)+old_area)
@@ -36,6 +39,7 @@ module Lib (integrate, integrate_nosort)
                 where (Point xm fm)=midpoint func left right
     integrate_iter_nosort _ _ points old_area = (points, old_area)
         
+    {-# INLINABLE integrate_nosort #-}
     integrate_nosort::(Fractional a, Ord a)=>(a->a)->[a]->a->a
     integrate_nosort func ticks eps=sum_area
             where 
