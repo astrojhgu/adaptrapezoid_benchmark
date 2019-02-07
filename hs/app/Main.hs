288cf5a96608a2940cb35c7469392f2b5ff5df47
Gu Junhua <jhgu@nao.cas.cn>
2019-02-07 22:52:22 +0800

    modified:   hs/app/Main.hs 	modified:   hs/src/Lib.hs

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -11,14 +11,10 @@ main :: IO ()
 main = do
   putStrLn "Validating precision"
   let precise_result=0.527038339761566009286263102166809763899326865179511011538
-  let result_sort=integrate foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)] 1e-10
+  let result=integrate foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)] 1e-10
   putStr "for sorted integration diff="
-  putStrLn $ show $ abs $ precise_result-result_sort
-  let result_nosort=integrate_nosort foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)] 1e-10
-  putStr "for non-sorted integration diff="
-  putStrLn $ show $ abs $ precise_result-result_nosort
+  putStrLn $ show $ abs $ precise_result-result
   defaultMain [
-    bgroup "integration" [ bench "sort"  $ whnf (integrate foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)]) 1e-10
-               , bench "no sort"  $ whnf (integrate_nosort foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)]) 1e-10
+    bgroup "integration" [ bench "neumaier"  $ whnf (integrate foo [0.0::Double, 1.0, 2.0, sqrt (8*pi)]) 1e-10
                ]
               ]
--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
-module Lib (integrate, integrate_nosort)
+module Lib (integrate)
     where
-    import Data.List
 
     data Point a=Point !a !a deriving Show -- x f
 
@@ -13,39 +12,34 @@ module Lib (integrate, integrate_nosort)
 
     area::(Fractional a)=>(Point a)->(Point a)->a
     area (Point x1 f1) (Point x2 f2)=(f1+f2)*(x2-x1)/2
-    
 
+    {-# INLINE neumaier_sum #-}
+    neumaier_sum::(Fractional a, Ord a)=>a->a->a->(a,a)
+    neumaier_sum !x !s !c=(new_s, new_c)
+            where
+                t=s+x
+                new_c = c + (update_comp x s t)
+                new_s=t
+                update_comp x1 s1 t1
+                    | (abs s1) >= (abs x1) = (s1 - t1) + x1
+                    | otherwise = (x1 - t1) + s1
+                
+    
     {-# INLINE integrate_iter #-}
-    integrate_iter::(Fractional a, Ord a)=>(a->a)->a->[Point a]->[a]->([Point a], [a])
-    integrate_iter func eps (left@(Point _ f1):right@(Point _ f2):others) !areas
-                | abs (f1+f2-fm*2) <= eps = integrate_iter func eps (right:others) ((area left right):areas)
-                | otherwise = integrate_iter func eps (left:(Point xm fm):right:others) areas
-                where (Point xm fm)=midpoint func left right
-    integrate_iter _ _ points areas = (points, areas)
+    integrate_iter::(Fractional a, Ord a)=>(a->a)->a->[Point a]->a->a->([Point a], a, a)
+    integrate_iter func eps (left@(Point _ f1):right@(Point _ f2):others) !old_area !old_comp
+                | abs (f1+f2-fm*2) <= eps = integrate_iter func eps (right:others) area1 comp1
+                | otherwise = integrate_iter func eps (left:(Point xm fm):right:others) old_area old_comp
+                where 
+                    (Point xm fm)=midpoint func left right
+                    (area1, comp1)=neumaier_sum (area left right) old_area old_comp
+    integrate_iter _ _ points old_area old_comp= (points, old_area, old_comp)
         
     {-# INLINABLE integrate #-}
     integrate::(Fractional a, Ord a)=>(a->a)->[a]->a->a
-    integrate func ticks eps = sum sorted_areas
+    integrate func ticks eps=sum_area+comp
             where 
                 points=fmap (\x->Point x $ func x) ticks
                 eps1=eps*4/((last ticks)-(head ticks))
-                areas=snd $ integrate_iter func eps1 points []
-                sorted_areas=sortBy (\a b->compare (abs a) (abs b)) areas
+                (_,sum_area,comp)=integrate_iter func eps1 points 0 0
     {-# SPECIALIZE integrate :: (Double -> Double) -> [Double] -> Double -> Double #-}
-
-    {-# INLINE integrate_iter_nosort #-}
-    integrate_iter_nosort::(Fractional a, Ord a)=>(a->a)->a->[Point a]->a->([Point a], a)
-    integrate_iter_nosort func eps (left@(Point _ f1):right@(Point _ f2):others) !old_area
-                | abs (f1+f2-fm*2) <= eps = integrate_iter_nosort func eps (right:others) ((area left right)+old_area)
-                | otherwise = integrate_iter_nosort func eps (left:(Point xm fm):right:others) old_area
-                where (Point xm fm)=midpoint func left right
-    integrate_iter_nosort _ _ points old_area = (points, old_area)
-        
-    {-# INLINABLE integrate_nosort #-}
-    integrate_nosort::(Fractional a, Ord a)=>(a->a)->[a]->a->a
-    integrate_nosort func ticks eps=sum_area
-            where 
-                points=fmap (\x->Point x $ func x) ticks
-                eps1=eps*4/((last ticks)-(head ticks))
-                sum_area=snd $ integrate_iter_nosort func eps1 points 0
-    {-# SPECIALIZE integrate_nosort :: (Double -> Double) -> [Double] -> Double -> Double #-}
